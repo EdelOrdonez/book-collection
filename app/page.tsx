6470dfae736d0eb91430331b05c18124fe485938
Edel Ordoñez <ordonez.e.15@hotmail.com>
2024-04-11 20:29:16 -0600

    Added books by month

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import Hero from "@/components/molecules/Hero";
 import BookList from "@/components/molecules/BookList";
+import { PinContainer, PinPerspective } from "@/components/ui/3d-pin";
+import { BackgroundGradient } from "@/components/ui/background-gradient";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      <p>Your text here</p>
       <BookList />
     </div>
   );
--- components/molecules/BookList.tsx
+++ components/molecules/BookList.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { BackgroundGradient } from "../ui/background-gradient";
+import { IconAppWindow } from "@tabler/icons-react";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { supabase } from "@/lib/initSupabase";
 
@@ -8,7 +12,7 @@ export default function BookList() {
     const { data: books } = await supabase
       .from("books")
       .select("*")
-      .order("title", true);
+      .order("id", true);
     setBooks(books);
   };
 
@@ -17,10 +21,24 @@ export default function BookList() {
   }, []);
 
   return (
-    <ul>
+    <div className="grid grid-cols-3 gap-4">
       {books.map((book) => (
-        <li key={book.id}>{book.title}</li>
+        <div key={book.id}>
+          <BackgroundGradient className="rounded-[22px] p-4 bg-white dark:bg-zinc-900">
+            <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+              {book.title}
+            </p>
+            <p className="text-sm text-neutral-600 dark:text-neutral-400">
+              The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
+              February 17, 2024. Your best opportunity to get these right now is
+              by entering raffles and waiting for the official releases.
+            </p>
+            <span className="text-xs bg-gradient-to-r from-[#00ccb1] to-[#7b61ff] text-white font-medium me-2 px-1 py-0.5 rounded border">
+              {book.month}
+            </span>
+          </BackgroundGradient>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 }
